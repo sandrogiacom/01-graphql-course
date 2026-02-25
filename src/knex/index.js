f5af8b05076e5d510e92153f38db008ceb9e82a1
sandro <sandrogiacom@gmail.com>
2026-02-25 18:29:37 -0300

    fix: update dependencies and default `knex` environment to 'development' fallback

--- src/knex/index.js
+++ src/knex/index.js
@@ -1,4 +1,4 @@
 import knexFn from 'knex';
 import knexfile from './knexfile';
 
-export const knex = knexFn(knexfile[process.env.NODE_ENV]);
+export const knex = knexFn(knexfile[process.env.NODE_ENV || 'development']);
